13dd43b4b61bbea3c87300e83791214807da744b
Yi Zeng <tsang990831@gmail.com>
2022-03-08 22:48:27 +0000

    cat, comment, admin models

--- cats/models.py
+++ cats/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class CatProfile(models.Model):
+    cid=models.AutoField(primary_key=True)
+    breed=models.CharField(max_length=50,null=False)
+    price_range=models.CharField(max_length=30)
+    friendliness=models.FloatField(default=3.0)
+    tidiness=models.FloatField(default=3.0)
+    fussiness=models.FloatField(default=3.0)
+    description = models.CharField(max_length=800,default="")
+
+    def __str__(self):
+        return self.breed
